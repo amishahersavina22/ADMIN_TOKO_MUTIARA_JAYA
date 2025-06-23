20d247bfac2cbdd81622d18cd97257a3ccce6ee0
amishahersavina22 <shashauji17@gmail.com>
2025-06-23 15:05:34 +0700

    ganti db

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ define('DB_NAME', 'toko_mutiara_jaya');
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // apache, root, '', 
- 
+
 // Check connection
 if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
